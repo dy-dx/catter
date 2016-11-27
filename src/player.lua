0dcb4c5d55dabdfeb70b14e9028e6e5d5bacf638
Chris Kim <christopherjkim@gmail.com>
2016-11-26 21:04:04 -0500

    fix diagonal movement, and add MAX_Y for player that isnt just screen height

--- src/player.lua
+++ src/player.lua
@@ -4,7 +4,8 @@ local BLOCK_SIZE = 64
 local BLOCK_W = 64
 local BLOCK_H = 50
 local yBlockOffset = (BLOCK_H - 44) / 2
-local INITIAL_POSITION = { x = BLOCK_W * 7, y = BLOCK_H * 12 + yBlockOffset }
+local MAX_Y = BLOCK_H * 12 + yBlockOffset
+local INITIAL_POSITION = { x = BLOCK_W * 7, y = MAX_Y }
 local SOUND = 'meow'
 
 function Player:new(image)
@@ -41,16 +42,13 @@ function Player:handleInput(dt)
         if love.keyboard.isDown("up") then
             self.y = self.y - BLOCK_H
             self.timeSinceMoved = 0
-        end
-        if love.keyboard.isDown("down") then
+        elseif love.keyboard.isDown("down") then
             self.y = self.y + BLOCK_H
             self.timeSinceMoved = 0
-        end
-        if love.keyboard.isDown("left") then
+        elseif love.keyboard.isDown("left") then
             self.x = self.x - BLOCK_W
             self.timeSinceMoved = 0
-        end
-        if love.keyboard.isDown("right") then
+        elseif love.keyboard.isDown("right") then
             self.x = self.x + BLOCK_W
             self.timeSinceMoved = 0
         end
@@ -67,7 +65,7 @@ function Player:update(dt, occupiedLog)
     self.x = math.max(0, self.x)
     self.x = math.min(dimensionWidth - self.width, self.x)
     self.y = math.max(0, self.y)
-    self.y = math.min(dimensionHeight - self.height, self.y)
+    self.y = math.min(MAX_Y, self.y)
 end
 
 return Player
